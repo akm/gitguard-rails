e5cdbc10ae50d45f73258fa2c1fdb1c3a6a3c2c6
akm <akm2000@gmail.com>
2016-07-29 17:39:16 +0900

    Don't guard on genrate gitguard:install

--- lib/gitguard/rails/commands_tasks.rb
+++ lib/gitguard/rails/commands_tasks.rb
@@ -5,11 +5,17 @@ module Gitguard
       TARGET_COMMANDS = %(plugin generate destroy).freeze
 
       def require_command!(command)
-        if Gitguard.enabled? && TARGET_COMMANDS.include?(command.to_s)
+        if target?(command.to_s)
           Gitguard.run("bin/rails #{command} #{argv.join(' ')}"){ super }
         else
           super
         end
+      end
+
+      def target?(command)
+        return false if argv.empty?
+        Gitguard.enabled? && TARGET_COMMANDS.include?(command) &&
+          (command == 'generate' ? !argv.include?('gitguard:install') : true)
       end
 
     end
