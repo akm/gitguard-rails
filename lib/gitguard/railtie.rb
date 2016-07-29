e642f084917ed8ccc282d187020ba676300c659b
akm <akm2000@gmail.com>
2016-07-29 16:16:06 +0900

    Fix indentation

--- lib/gitguard/railtie.rb
+++ lib/gitguard/railtie.rb
@@ -4,13 +4,13 @@ require 'rails'
 require 'rails/commands/commands_tasks'
 
 module Gitguard
-    class Railtie < ::Rails::Railtie
-      generators do
-        ::Rails::CommandsTasks.__send__(:prepend, ::Gitguard::Rails::CommandsTasks)
-      end
-
-      rake_tasks do
-        ::Rake::Application.__send__(:prepend, ::Gitguard::RakeApplication)
-      end
+  class Railtie < ::Rails::Railtie
+    generators do
+      ::Rails::CommandsTasks.__send__(:prepend, ::Gitguard::Rails::CommandsTasks)
     end
+
+    rake_tasks do
+      ::Rake::Application.__send__(:prepend, ::Gitguard::RakeApplication)
+    end
+  end
 end
