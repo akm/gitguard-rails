23a0be3a93243b3195c8ce87789401182c7b2a4b
akm <akm2000@gmail.com>
2016-08-01 15:44:56 +0900

    Suppress the output for test

--- spec/support/cmd.rb
+++ spec/support/cmd.rb
@@ -2,9 +2,7 @@ module Cmd
   module_function
 
   def run(cmd)
-    puts cmd
-    r = system("#{cmd}", out: "/dev/null", err: "/dev/null")
-    # puts(r ? "\e[32mSuccess: #{cmd}\e[0m" : "\e[31mError: #{cmd}\e[0m")
-    r
+    puts(cmd) if ENV['VERBOSE'] =~ /true|yes|on|1/i
+    system("#{cmd}", out: "/dev/null", err: "/dev/null")
   end
 end
